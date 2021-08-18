2c17f80777ec69660db40e8995993033f8d42661
xplim <75422036+xplim@users.noreply.github.com>
2021-08-18 22:25:16 +0800

    Refactor codes to `Directory` and `MenuItem` components

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,41 +1,12 @@
 import React from 'react';
 
+import Directory from '../../components/directory/directory.component';
+
 import './homepage.styles.scss';
 
 const HomePage = () => (
   <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <div className="title">Hats</div>
-          <span className="subtitle">Shop now</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <div className="title">Jackets</div>
-          <span className="subtitle">Shop now</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <div className="title">Sneakers</div>
-          <span className="subtitle">Shop now</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <div className="title">Womens</div>
-          <span className="subtitle">Shop now</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <div className="title">Mens</div>
-          <span className="subtitle">Shop now</span>
-        </div>
-      </div>
-    </div>
+    <Directory />
   </div>
 );
 
