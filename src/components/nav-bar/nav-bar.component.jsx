9ba35a14531a6edb7410ff9318a8bb9e667eee92
xplim <75422036+xplim@users.noreply.github.com>
2021-09-02 19:25:31 +0800

    Add `CartDropdown` component

--- src/components/nav-bar/nav-bar.component.jsx
+++ src/components/nav-bar/nav-bar.component.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './nav-bar.styles.scss';
@@ -31,6 +32,7 @@ const NavBar = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 );
 
